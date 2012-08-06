de57b9a44179c90c6f921c1410d2cd9cbc6bc4bd
Daniel Beardsley <daniel@ifixit.com>
2012-08-05 22:33:14 -0700

    Refactor and comment
    Swap the if statements so usage help is on top

--- pipe-to-graphite.sh
+++ pipe-to-graphite.sh
@@ -1,14 +1,73 @@
 #!/bin/bash
 
+##
+# Use this to send the output of any command or script to graphite at regular
+# intervals in the background.
+#
+# Run this script with no arguments to see proper usage.
+#
+# Author: Daniel Beardsley: daniel@ifixit.com
+# Origin: git@gist.github.com:3271040.git
+##
+
+# edit these to match your graphite setup
 GRAPHITE_SERVER=localhost
 GRAPHITE_PORT=2003
-GRAPHITE_INTERVAL=10
+GRAPHITE_INTERVAL=10 # in seconds
 
-action="$1"
+# Normal usage just passes the command as the only parameter
+# This checks if we're on a recursive call.
+if [ "$1" != "report-to-graphite" ]; then
+   command="$1"
 
-if [ "$action" == "report-to-graphite" ]; then
+   echo -n "Running '$command' as a test.. " >&2
+   test_output=$($command 2>&1)
+   test_return=$?
+
+   if [ "$test_output" == "" ] || [ "$test_return" != "0" ]; then
+      (
+      echo "FAILED"
+      echo
+      echo "Usage:"
+      script="`basename $0`"
+      echo "  $script '/command/to/run' >> /var/log/some-stats.log "
+      echo
+      echo "/command/to/run must:"
+      echo " * echo 'name number' pairs separated by newlines"
+      echo " * have an exit code of 0"
+      ) >&2
+      exit 1
+
+   else
+      echo "SUCCESS" >&2
+      echo
+   fi
+
+   script="$0"
+   # If we are connected to a terminal redirect stdout to /dev/null
+   # so nohup doesn't complain and send it to nohup.out
+   if [ -t 1 ] ; then
+      echo "Redirecting stdout to /dev/null so it doesn't mess up your" >&2
+      echo "terminal.  Redirect it somewhere else if you wan't to save it." >&2
+      echo >&2
+      nohup $script 'report-to-graphite' "$command" >/dev/null &
+   else
+      nohup $script 'report-to-graphite' "$command" &
+   fi
+   pid=$!
+   (
+   echo "Command: $command"
+   echo "is being piped to graphite every $GRAPHITE_INTERVAL seconds"
+   echo "Background PID: $pid"
+   ) >&2
+
+# Internal usage passes the action as the first parameter
+# If we get here, we're running from the nohup command above
+else
+   # first parameter was the action, second is the command
    command="$2"
 
+   # The actual sleep, report, sleep loop
    while true; do
       # Do it in a backgrounded subshell so we can move
       # directly on to sleeping for $GRAPHITE_INTERVAL
@@ -16,9 +75,12 @@ if [ "$action" == "report-to-graphite" ]; then
          # Get a timestamp for sending to graphite
          ts=`date +%s`
 
+         # Run the provided command and capture stdout
          output=$($command)
          exit_status=$?
          
+         # If the command didn't succeed, prepend the output with a failure
+         # message and don't send it to graphite.
          if [ "$exit_status" != "0" ]; then
             err="FAILED: exit code = $exit_status, not reported to graphite"
             output="$err
@@ -40,48 +102,6 @@ $output"
       ) &
       sleep $GRAPHITE_INTERVAL
    done;
-   exit 0
 fi 
 
-command="$1"
-
-echo -n "Running '$command' as a test.. " >&2
-test_output=$($command 2>&1)
-test_return=$?
-
-if [ "$test_output" == "" ] || [ "$test_return" != "0" ]; then
-   (
-   echo "FAILED"
-   echo
-   echo "Usage:"
-   script="`basename $0`"
-   echo "  $script '/command/to/run' >> /var/log/some-stats.log "
-   echo 
-   echo "/command/to/run must:"
-   echo " * echo 'name number' pairs separated by newlines"
-   echo " * have an exit code of 0"
-   ) >&2
-   exit 1
-
-else
-   echo "SUCCESS" >&2
-   echo
-   script="$0"
-   # If we are connected to a terminal redirect stdout to /dev/null
-   # so nohup doesn't send it to nohup.out
-   if [ -t 1 ] ; then
-      echo "Redirecting stdout to /dev/null so it doesn't mess up your" >&2
-      echo "terminal.  Direct it somewhere else if you wan't to save it." >&2
-      echo >&2
-      nohup $script 'report-to-graphite' "$command" >/dev/null &
-   else
-      nohup $script 'report-to-graphite' "$command" &
-   fi
-   pid=$!
-   (
-   echo "Started piping output of '$command' to graphite"
-   echo "Background PID: $pid"
-   ) >&2
-   exit 0
-fi
 
