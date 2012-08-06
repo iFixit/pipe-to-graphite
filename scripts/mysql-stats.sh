b42b735932863dfc75162a76eea1dd1aa14362a6
Daniel Beardsley <daniel@ifixit.com>
2012-08-06 01:33:06 -0700

    Mysql-stats: fix bug in order of sed commands
    Prepending must happen after the _ replacement

--- scripts/mysql-stats.sh
+++ scripts/mysql-stats.sh
@@ -20,5 +20,5 @@ awk "$awk_command" |
 tr '[A-Z]' '[a-z]' |
 # Prepend 'mysql.' and turn a few instances of 'name_' into 'name.' mainly
 # so they are grouped in the Graphite UI
-sed -e "s/^/mysql./" -re "s/^(com|handler|innodb|key|qcache|select|sort|threads)_/\1./"
+sed -re "s/^(com|handler|innodb|key|qcache|select|sort|threads)_/\1./" -e "s/^/mysql./"
 
