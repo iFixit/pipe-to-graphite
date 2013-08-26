265010aa4a47173c6bb0929c4c6d038c48c2500f
Daniel Beardsley <daniel@ifixit.com>
2013-08-26 00:08:03 -0700

    Add ping-time script
    Include source dest and prefix in stat names

---
+++ scripts/ping.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SRC=`hostname`
+PREFIX="${PREFIX:-ping}.${SRC/./_}.${DEST/./_}"
+# Set the DEST variable to the destination address
+output=`ping -q -c 10 -i 0.5 $DEST | tail -2`
+percent_loss=`echo $output | grep -oP "[0-9]+%"`
+avg_time=`echo $output | grep -oP "/[.0-9]+/"`
+echo "$PREFIX.percent_loss $percent_loss"
+echo "$PREFIX.time ${avg_time//\/}"
+
