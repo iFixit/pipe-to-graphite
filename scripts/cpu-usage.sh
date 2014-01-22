f0608e44d2caf63f132ad99d87032fb549098d0c
Daniel Beardsley <daniel@ifixit.com>
2014-01-22 14:29:02 -0700

    cpu-usage: include hostname... duh

--- scripts/cpu-usage.sh
+++ scripts/cpu-usage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 cpu_line=$(top -b -n 1 | grep ^Cpu)
+hostname=$(hostname)
 
 extract() {
    local name="$1"
    local id="$2"
-   echo -n "$name "
+   echo -n "servers.${hostname//./_}.$name "
    echo "$cpu_line" | sed -r "s/^.* ([0-9.]+)%$id.*$/\1/"
 }
 
